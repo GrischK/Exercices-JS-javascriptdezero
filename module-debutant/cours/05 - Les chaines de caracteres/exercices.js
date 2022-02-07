a630d29624a4aa78d652fc2f69c08dce5ecd9069
GrischK <grischka.gorski@laposte.net>
2022-02-07 14:56:30 +0100

    exercies 05 fini

--- module-debutant/cours/05 - Les chaines de caracteres/exercices.js
+++ module-debutant/cours/05 - Les chaines de caracteres/exercices.js
@@ -9,23 +9,23 @@
  */
 
 // Exercice 0
-
+console.log("JavaScript est facile");
 // Exercice 1
-
+console.log("JavaScript c'est facile");
 // Exercice 2
-
+console.log(`C'est Lao Tseu qui a dit : "L'échec est le fondement de la réussite"`);
 // Exercice 3
-
+console.log("Afficher un \\ n'est pas si évident");
 // Exercice 4
-
+console.log("Ceci est la première ligne\nCeci est la deuxième ligne");
 // Exercice 5
-
+console.log("Un jour le JavaScript n'aura plus de secrets pour moi\nLire du code ne sera plus une gageure mais une joie\nMes doigts seront habitués à tous ces symboles particuliers\nEt ma créativité de développeur pourra enfin s'exprimer");
 // Chassez le bogue 0
 // Retirez les commentaires devant le code de Tom pour commencer
-// console.log("Bonjour' + 'Bob' + ', aujourd'hui nous faisons une promo 'la fidélité paye' !");
+console.log("Bonjour " + 'Bob' + ', aujourd\'hui nous faisons une promo "la fidélité paye" !');
 
 // Chassez le bogue 1
 // Retirez les commentaires devant le code de Tom pour commencer (les 3 lignes ci-dessous)
-// console.log('        Bonjour' + 'Bob' + ",  \
-// \
-// Aujourd\'hui nous faisons une promo \"la fidélité paye\" !");
+console.log('\t\tBonjour ' + 'Bob' + `,
+
+Aujourd'hui nous faisons une promo "la fidélité paye" !`);
