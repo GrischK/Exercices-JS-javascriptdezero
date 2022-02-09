5654b7be71d7e6e60f9c065365ddce152f873767
GrischK <grischka.gorski@laposte.net>
2022-02-09 14:06:27 +0100

    exercices 13 boucles finis

--- module-debutant/cours/11 - Mini projet 1/cours.js
+++ module-debutant/cours/11 - Mini projet 1/cours.js
@@ -3,7 +3,7 @@
 // Exemple 1 : pour une durée de RDV de 15 minutes
 
 // Début du RDV : 15h40
-let debutRdvHeures = 15;
+let debutRdvHeures = 16;
 let debutRdvMinutes = 40;
 
 // Fin de journée : 16h30
@@ -11,18 +11,28 @@ let finJourneeHeures = 16;
 let finJourneeMinutes = 30;
 
 // Durée du RDV : 15 minutes
-let duree = 15;
+let duree = 30;
 
 // ETAPE 1 : Calcul de la fin du RDV
 let finRdvHeures = debutRdvHeures;
 let finRdvMinutes = debutRdvMinutes + duree;
 
+
 // ETAPE 2 : Est-ce que la fin du RDV est avant la fin de la journée ?
-if (finRdvHeures < finJourneeHeures) {
+if (finRdvHeures < finJourneeHeures || (finRdvHeures === finJourneeHeures && finRdvMinutes < finJourneeMinutes)) {
   console.log("RDV OK");
 } else {
   console.log("RDV impossible");
 }
 
+if (finRdvMinutes >= 60) {
+  finRdvHeures = finRdvHeures + 1;
+  finRdvMinutes = finRdvMinutes - 60;
+}
+
 // ETAPE 3 : Affichage de l'heure de fin du RDV
-console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes);
+if (finRdvMinutes < 10) {
+console.log("Le RDV se terminera à " + finRdvHeures + "h0" + finRdvMinutes); 
+} else {
+  console.log("Le RDV se terminera à " + finRdvHeures + "h" + finRdvMinutes); 
+}
